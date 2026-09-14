bd7f4ce8119c36c0990361e7d8e9ef8cd36798ed
Manoah Gilles <mwa.gilles@gmail.com>
2026-09-14 10:11:39 +0200

    Dev du formulaire de contact

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,32 @@
+emailjs.init("wGjdf46DQmql1DUKB");
+
+const formulaire = document.getElementById('form-soleilka');
+
+if (formulaire) {
+    formulaire.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+
+        emailjs.sendForm('mail-auto_test1', 'template_huqsuz9', this)
+            .then(function() {
+                alert('Bienvenue chez Soleil Ka ! Ta demande est envoyée.');
+                formulaire.reset();
+            }, function(erreur) {
+                alert('Oups, petit couac technique : ' + JSON.stringify(erreur));
+            });
+    });
+}
+
 const header = document.querySelector('.header');
 
-// On écoute l'événement de défilement (scroll) sur la fenêtre
-window.addEventListener('scroll', () => {
-  // Si on a défilé de plus de 50 pixels vers le bas
-  if (window.scrollY > 50) {
-    header.classList.add('header-scrolled'); // On rend translucide
-  } else {
-    header.classList.remove('header-scrolled'); // On remet solide en haut de page
-  }
-});
+
+if (header) {
+    window.addEventListener('scroll', () => {
+
+      if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+      } else {
+        header.classList.remove('header-scrolled');
+      }
+    });
+}
