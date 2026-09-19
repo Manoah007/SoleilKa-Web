5ec5aa1c374329fbc15b48fd580ea33906d43d64
Manoah Gilles <mwa.gilles@gmail.com>
2026-09-19 20:57:18 +0200

    Débug Menu vs mobile

--- static/js/main.js
+++ static/js/main.js
@@ -42,9 +42,15 @@ if (header) {
     Crée un menu latérale pour les résolutation plus petites (ex: mobile)
 ===========================================================================*/
 const burgerBtn = document.getElementById('burger-btn');
-const nav = document.querySelector('.header-nav-mobile'); /* Modification ici */
+const nav = document.querySelector('.header-nav-mobile');
 
 burgerBtn.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burgerBtn.classList.toggle('toggle');
+});
+
+burgerBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burgerBtn.classList.toggle('toggle');
+    document.body.classList.toggle('no-scroll'); /* Bloque ou débloque le défilement du site */
 });
